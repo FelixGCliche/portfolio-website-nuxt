aa606428c7fbb82e3f5b2ae93bd771ac6311b302
Felix <felix.gagne.cliche@gmail.com>
2022-02-26 01:45:20 -0500

    fix screen resolution

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,21 @@ import { resolve} from 'path'
 export default defineNuxtConfig ({
   target: 'static',
 
+  head: {
+    title: 'Félix Gagné Cliche',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
 
   alias: {
     'style': resolve(__dirname, './assets/style'),
