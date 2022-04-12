862f40735378d2d8d5fa4a3b161082ca9c0bebb4
Felix <felix.gagne.cliche@gmail.com>
2022-04-12 04:36:57 -0400

    update formatting scripts

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,4 @@
 module.exports = {
   extends: ['@nuxtjs/eslint-config-typescript', 'prettier'],
-  plugins: ['prettier'],
-  rules: {
-    'prettier/prettier': ['error']
-  }
+  plugins: ['prettier']
 }
