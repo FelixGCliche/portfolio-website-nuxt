86a85355abb631cf54c2245059419e3dc2165a1d
Félix Gagné Cliche <felix.gagne.cliche@gmail.com>
2022-07-27 12:27:34 -0400

    update home screen

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,27 @@
 <template>
-  <div class="grid">
-    <div class="section">
-      <SectionAbout />
+  <main class="home-main">
+    <div class="grid">
+      <div class="section">
+        <HomeAboutSection />
+      </div>
+      <div class="profile">
+        <img
+          class="img-responsive"
+          src="~img/profile.png"
+          alt="profile picture"
+        />
+      </div>
+      <div class="logo-container">
+        <img src="~img/portfolio_logo.svg" alt="logo" />
+      </div>
     </div>
-    <div class="profile">
-      <img class="img-responsive" src="assets\img\profile.png" alt="" />
-    </div>
-  </div>
+    <img
+      class="home-bg"
+      src="~img/bg_image.png"
+      alt="home background"
+      mask="url(#flat)"
+    />
+  </main>
 </template>
 
 <script lang="ts" setup></script>
@@ -14,38 +29,59 @@
 <style lang="scss" scoped>
 @use 'sass:map';
 
-@function getOverlapStart($size) {
+@function get-overlap-start($size) {
   $base: calc(map.get(layout.$columns, $size) + 1);
   @return calc($base * -1);
 }
 
-@function getProfileOverlap($size) {
+@function get-profile-overlap($size) {
   $base: calc(map.get(layout.$columns, $size) / 2);
   @return calc($base + 1);
 }
 .grid {
   @include layout.layout-grid;
+  grid-template-rows: 1fr auto auto 1fr;
   height: 100%;
 }
 .section {
   @include layout.responsive-cell-base {
-    grid-row: 2;
-    z-index: 2;
+    grid-row: 3;
+    z-index: 3;
   }
 }
 .profile {
   @include layout.responsive-cell-base {
-    grid-row-start: 1;
+    grid-row-start: 2;
     grid-row-end: span 3;
-    z-index: 1;
+    align-self: start;
+    z-index: 2;
   }
+}
+.logo-container {
+  grid-row: 2;
+  z-index: 1;
+  width: 100%;
+  height: auto;
+  align-self: end;
+}
+.home-bg {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  mask-image: linear-gradient(
+    180deg,
+    rgba(0, 0, 0, 1) 0%,
+    rgba(0, 0, 0, 0) 66%
+  );
 }
 
 @each $size in map.keys(layout.$breakpoints) {
   @include layout.media-query($size) {
     $columns: map.get(layout.$columns, $size);
+
     .section {
-      grid-column-start: getOverlapStart($size);
+      grid-column-start: get-overlap-start($size);
       @if $size == 'xsmall' {
         grid-column-end: span $columns;
       } @else if $size == 'large' {
@@ -54,8 +90,18 @@
         grid-column-end: span calc(($columns / 2) + 1);
       }
     }
+    .logo-container {
+      grid-column-start: get-overlap-start($size);
+      @if $size == 'xsmall' {
+        grid-column-end: span $columns - 1;
+      } @else if $size == 'large' {
+        grid-column-end: span calc($columns / 2) + 2;
+      } @else {
+        grid-column-end: span calc(($columns / 2) + 1);
+      }
+    }
     .profile {
-      grid-column-start: span getProfileOverlap($size);
+      grid-column-start: span get-profile-overlap($size);
       grid-column-end: -1;
     }
   }
