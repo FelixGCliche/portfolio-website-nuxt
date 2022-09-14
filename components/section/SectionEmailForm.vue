2c2610b25dec5aaf8c436e37a08726ea0867f5a6
Félix Gagné Cliche <felix.gagne.cliche@gmail.com>
2022-09-14 19:55:52 -0400

    add submit button

--- components/section/SectionEmailForm.vue
+++ components/section/SectionEmailForm.vue
@@ -1,5 +1,6 @@
 <template>
-  <form class="form-email" action="">
+  <form name="form-email" class="form-email">
+    <input type="hidden" name="form-name" value="form-email" />
     <InputTextField
       input-name="name"
       input-label="Name"
@@ -15,17 +16,42 @@
       input-label="Message"
       :input-required="true"
     />
+    <button class="button-submit" type="submit">
+      <label class="label" for="button-submit">Submit</label>
+    </button>
   </form>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts"></script>
 
 <style lang="scss" scoped>
+@use 'sass:map';
+
 .form-email {
   display: grid;
   grid-template-rows: repeat(3, max-content);
   gap: 4rem;
   width: 100%;
   height: 100%;
+}
+
+.button-submit {
+  min-width: 4rem;
+  background: transparent;
+  padding: 0.25rem 1rem;
+  color: theme.$on-background;
+  border: none;
+  width: fit-content;
+  height: fit-content;
+  border: 4px solid theme.$primary;
+
+  &:hover {
+    @include theme.hover(theme.$on-background) {
+      border-color: theme.$secondary;
+    }
+  }
+  &:active {
+    @include theme.pressed(theme.$secondary);
+  }
 }
 </style>
