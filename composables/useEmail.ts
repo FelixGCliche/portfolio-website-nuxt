34396202762e018732de3f2c29500b65234613ca
Félix Gagné Cliche <felix.gagne.cliche@gmail.com>
2022-09-14 23:05:28 -0400

    hotfix send email post

--- composables/useEmail.ts
+++ composables/useEmail.ts
@@ -3,7 +3,7 @@ import emailjs from 'emailjs-com'
 export function useEmail(form: HTMLFormElement) {
   emailjs.sendForm('portfolio', 'default', form, 'fQsPv5E3xcb_a9l0T').then(
     () => {
-      // window.location.reload()
+      window.location.reload()
     },
     error => {
       alert(`Message not sent ${error}`)
